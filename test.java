16d6ad66ff47f196d4c9dd729234ecdb3cba043b
SahasTc3005789 <158039212+SahasTc3005789@users.noreply.github.com>
2024-01-30 14:33:20 +0000

    Add files via upload

---
+++ test.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class CountDuplicates {
+
+    public static void main(String[] args) {
+        String a = "hello, world";
+        int i = new CountDuplicates().duplicateDuplicates(a);
+
+        System.out.println(i);
+    }
+
+    public int duplicateDuplicates(String string) {
+        // Add your code here.
+        return 0;
+    }
+}
